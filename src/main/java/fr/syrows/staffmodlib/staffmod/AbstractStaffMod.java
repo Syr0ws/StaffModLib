db2de10c14dacee5a647112c288b2462603c8d4f
Syrows <clement.gho@gmail.com>
2021-01-28 20:45:43 +0100

    Update

--- src/main/java/fr/syrows/staffmodlib/staffmod/AbstractStaffMod.java
+++ src/main/java/fr/syrows/staffmodlib/staffmod/AbstractStaffMod.java
@@ -1,35 +1,34 @@
 package fr.syrows.staffmodlib.staffmod;
 
 import fr.syrows.staffmodlib.StaffModManager;
-import fr.syrows.staffmodlib.data.Data;
 import fr.syrows.staffmodlib.data.PlayerData;
 import fr.syrows.staffmodlib.staffmod.items.StaffModItem;
 import org.bukkit.entity.Player;
-
-import java.util.ArrayList;
-import java.util.Collection;
-import java.util.Collections;
-import java.util.List;
-import java.util.Optional;
-import java.util.stream.Collectors;
+import org.bukkit.inventory.PlayerInventory;
 
 public abstract class AbstractStaffMod implements StaffMod {
 
+    public static final int MAX_ITEMS = 9;
+
     private final StaffModManager manager;
-    private final List<StaffModItem> items = new ArrayList<>();
 
     private Player player;
-    private PlayerData playerData;
+    private PlayerData data;
 
     public AbstractStaffMod(StaffModManager manager) {
         this.manager = manager;
     }
 
+    public abstract void registerItems(Player player);
+
     public abstract PlayerData createPlayerData();
 
     public void registerItem(StaffModItem item) {
+
+        if(item.getSlot() < 0 || item.getSlot() >= MAX_ITEMS)
+            throw new IllegalArgumentException("Slot must be between 0 and 8.");
+
         item.onRegister();
-        this.items.add(item);
     }
 
     @Override
@@ -38,14 +37,12 @@ public abstract class AbstractStaffMod implements StaffMod {
         this.player = player;
         this.manager.setStaffMod(player, this);
 
+        this.data = this.createPlayerData();
+        this.data.save(player);
+        this.data.clear(player);
+
         this.registerItems(player);
-
-        this.playerData = this.createPlayerData();
-        this.playerData.save(player);
-        this.playerData.clear(player);
-
-        this.getModItems()
-                .forEach(item -> player.getInventory().setItem(item.getSlot(), item.getItemStack()));
+        this.setItems();
     }
 
     @Override
@@ -54,24 +51,19 @@ public abstract class AbstractStaffMod implements StaffMod {
         this.player = null;
         this.manager.removeStaffMod(player);
 
-        this.items.forEach(StaffModItem::onUnregister);
+        this.getItems().forEach(StaffModItem::onUnregister);
 
-        this.playerData.clear(player);
-        this.playerData.restore(player);
+        this.data.clear(player);
+        this.data.restore(player);
     }
 
-    @Override
-    public Collection<StaffModItem> getModItems() {
-        return Collections.unmodifiableList(this.items);
+    protected void setItems() {
+        PlayerInventory inventory = this.player.getInventory();
+        this.getItems().forEach(item -> inventory.setItem(item.getSlot(), item.getItemStack()));
     }
 
-    @Override
-    public Data getPlayerData() {
-        return this.playerData;
-    }
-
-    public StaffModManager getStaffModManager() {
-        return this.manager;
+    public PlayerData getPlayerData() {
+        return this.data;
     }
 
     public Player getPlayer() {
